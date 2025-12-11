bc811f9a4054b3b473032bbdd982f69cb37cb62e
engrabdulmalik <engrabdulmaliq@gmail.com>
2025-12-11 11:48:54 +0500

    new

--- backendAPI/admin.py
+++ backendAPI/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import Drinks
+from .models import *
 
 # Register your models here.
 admin.site.register(Drinks)
+admin.site.register(College)
+admin.site.register(Principal)
+admin.site.register(Subject)
+admin.site.register(Teacher)
--- backendAPI/models.py
+++ backendAPI/models.py
@@ -6,3 +6,40 @@ class Drinks(models.Model):
 
     def __str__(self):
         return self.name
+
+class College(models.Model):
+    CollegeID=models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+    location = models.CharField(max_length=200)
+    strength = models.IntegerField()
+    website=models.URLField()
+
+    def __str__(self):
+        return self.name
+class Principal(models.Model):
+    PrincipalID=models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+    age = models.IntegerField()
+    email=models.EmailField()
+    college = models.OneToOneField(College, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+class Subject(models.Model):
+    SubjectID=models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+    code = models.CharField(max_length=20)
+    credits=models.IntegerField()
+    def __str__(self):
+        return self.name
+
+class Teacher(models.Model):
+    TeacherID=models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+    age = models.IntegerField()
+    email=models.EmailField()
+    college = models.ForeignKey(College, on_delete=models.CASCADE)
+    subjects = models.ManyToManyField(Subject)
+
+    def __str__(self):
+        return self.name
